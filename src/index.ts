b7fc09507bcc1d268a4ac7fcfb478b71e53e43fd
Francis Wibisono <code@franciswibisono.com>
2025-04-22 16:32:52 +0700

    feat: add cors and health-check

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { Elysia } from "elysia";
+import { cors } from '@elysiajs/cors';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+    .use(cors({
+      origin: '*' // Allow requests from any origin
+    }))
+    .get("/", () => "Hello Elysia")
+    .get("/health-check", () => "Backend running OK!").listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
